592d9448c142ef48783f80aa7018b6169797ce61
atul-gairola <atulgairola08@gmail.com>
2021-05-01 22:38:15 +0530

    Set up base component

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC0rQgaRtjIxO473mKtCZNLirrZ_aFqgJA",
+  authDomain: "whatsapp-clone-af2cd.firebaseapp.com",
+  projectId: "whatsapp-clone-af2cd",
+  storageBucket: "whatsapp-clone-af2cd.appspot.com",
+  messagingSenderId: "863230303087",
+  appId: "1:863230303087:web:908e0f23e24e302462a69a",
+  measurementId: "G-NFYKLBX2E5",
+};
