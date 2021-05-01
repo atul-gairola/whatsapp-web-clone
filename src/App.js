19c62b0813803e1668c15e092df432ce02fae768
atul-gairola <atulgairola08@gmail.com>
2021-05-02 00:43:10 +0530

    Create InitialDisplayBox

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core";
 
+import InitialDisplayBox from "./components/InitialDisplayBox";
+
 const useStyles = makeStyles({
-  app: {
-    width: "100vw",
-    height: "100vh",
-    overflow: "hidden",
-    position: "relative",
-    background: "linear-gradient(180deg, #009688 17%, #d6dbd8 17%)",
-  },
   main_container: {
     width: "100%",
     maxWidth: "1396px",
-    height: "100%",
-    margin: "20px 0",
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
+    height: "calc(100vh - 40px)",
+    margin: "20px auto 0 auto",
     boxShadow: "0 1px 1px 0 rgba(0,0,0,.06),0 2px 5px 0 rgba(0,0,0,.2)",
     background: "#fff",
     display: "grid",
-    gridTemplateColumns: "30% 70%"
+    gridTemplateColumns: "30% 70%",
   },
+  inside_container: {},
 });
 
 function App() {
   const classes = useStyles();
   return (
-    <div className={classes.app}>
+    <div className={classes.app} >
       <div className={classes.main_container}>
         <section>hj</section>
-        <section>hello</section>
+        <section>
+          <InitialDisplayBox />
+        </section>
       </div>
     </div>
   );
---
+++ src/components/InitialDisplayBox.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+import InitialImg from "../images/initial.png";
+import LaptopMacIcon from "@material-ui/icons/LaptopMac";
+
+const useStyles = makeStyles({
+  container: {
+    width: "100%",
+    height: "100%",
+    background: "#f8f9fa",
+    display: "grid",
+    placeItems: "center",
+    textAlign: "center",
+    "& h1": {
+      color: "#525252",
+    },
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 300,
+    marginTop: 28,
+  },
+  subtitle: {
+    fontWeight: 400,
+    color: "rgba(0, 0, 0, 0.45)",
+    fontSize: 17,
+    marginTop: 18,
+    maxWidth: 500,
+    paddingBottom: 40,
+    borderBottom: "1.6px solid rgba(0,0,0,0.08)",
+  },
+  image: {
+    width: "300px",
+  },
+  afterline: {
+    display: "grid",
+    alignItems: "center",
+    gridTemplateColumns: "auto auto",
+    margin: "20px auto",
+    justifyContent: "center",
+    color: "rgba(0, 0, 0, 0.45)",
+    fontSize: 16,
+    fontWeight: 400,
+    gridGap: 8,
+    "& a": {
+        color: "#07bc4c",
+        cursor: "pointer",
+        textDecoration: "none"
+    }
+  },
+});
+
+function InitialDisplayBox() {
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <div className={classes.inside_container}>
+        <img className={classes.image} src={InitialImg} alt="initial" />
+        <h1 className={classes.title}>Welcome to WhatsApp Web</h1>
+        <p className={classes.subtitle}>
+          Chat in realtime with your family, friends and colleagues and never
+          miss out on those who really matter.
+        </p>
+        <div className={classes.afterline}>
+          <LaptopMacIcon style={{ marginTop: "3px" }} fontSize="small" />
+          <p>
+            Original WhatsApp for Windows. <a href="#" >Get it here.</a>{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default InitialDisplayBox;
