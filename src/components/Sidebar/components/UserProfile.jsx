c72ad11fec33565a05779cee52a888580412c050
atul-gairola <atulgairola08@gmail.com>
2021-05-11 19:26:49 +0530

    Add Form input fields in UserProfile

--- src/components/Sidebar/components/UserProfile.jsx
+++ src/components/Sidebar/components/UserProfile.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles, IconButton } from "@material-ui/core";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import { useAuth } from "../../../contexts/AuthContext";
 import CameraAltIcon from "@material-ui/icons/CameraAlt";
+import EditIcon from "@material-ui/icons/Edit";
+import DoneIcon from "@material-ui/icons/Done";
 
 const useStyles = makeStyles({
   container: {
@@ -74,15 +76,76 @@ const useStyles = makeStyles({
     background: "rgba(74, 74, 74, 0.7)",
     opacity: 0,
     "&:hover": {
-        opacity: 0.9
+      opacity: 0.9,
     },
-    transition: "0.95s"
+    transition: "0.95s",
+  },
+  inputContainer: {
+    background: "#fff",
+    boxShadow: "0 1px 3px rgba(0,0,0,0.08)",
+    padding: "20px 40px",
+    "& label": {
+      fontSize: 16,
+      color: "#009688",
+      fontWeight: 500,
+    },
+  },
+  inputDiv: {
+    display: "flex",
+    color: "#919191",
+    "& input": {
+      flexGrow: 1,
+      marginRight: 10,
+      border: "none",
+      fontSize: 17,
+      color: "#4a4a4a",
+      "&:disabled": {
+        backgroundColor: "transparent",
+      },
+    },
+    "& > button": {
+      color: "#919191",
+      cursor: "pointer",
+      background: "transparent",
+      border: "none",
+    },
+  },
+  active: {
+    borderBottom: "2px solid #00bfa5",
   },
 });
 
 function UserProfile() {
   const classes = useStyles();
   const { currentUser } = useAuth();
+
+  //   states
+  const [values, setValues] = useState({
+    name: currentUser.name,
+    about: currentUser.about || "",
+  });
+
+  const [editable, setEditable] = useState({
+    name: false,
+    about: false,
+  });
+
+  //   handlers
+
+  const handleEditToggle = (str) => {
+    setEditable((prev) => ({
+      ...prev,
+      [str]: !prev[str],
+    }));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
   return (
     <div className={classes.container}>
@@ -104,16 +167,66 @@ function UserProfile() {
         <div className={classes.imgContainer}>
           <div className={classes.display}>
             <div className={classes.overlay}>
-            <div>
-              <CameraAltIcon />
-              <p style={{fontSize: 13, paddingTop: 7}}>CHANGE <br /> PROILE PHOTO</p>
+              <div>
+                <CameraAltIcon />
+                <p style={{ fontSize: 13, paddingTop: 7 }}>
+                  CHANGE <br /> PROILE PHOTO
+                </p>
               </div>
             </div>
             <img src={currentUser.img} alt={currentUser.name} />
           </div>
         </div>
-        <div className={classes.nameContainer}></div>
-        <div className={classes.aboutContainer}></div>
+        <div className={classes.nameContainer}>
+          <div className={classes.inputContainer}>
+            <div style={{ marginBottom: 19 }}>
+              <label>Your Name</label>
+            </div>
+            <div
+              className={`${classes.inputDiv} ${
+                editable.name && classes.active
+              }`}
+            >
+              <input
+                autoComplete="off"
+                value={values.name}
+                onChange={handleInputChange}
+                name="name"
+                type="text"
+                placeholder="Write your name"
+                disabled={!editable.name}
+              />
+              <button onClick={() => handleEditToggle("name")} title="Edit">
+                {editable.name ? <DoneIcon /> : <EditIcon />}
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className={classes.aboutContainer}>
+          <div className={classes.inputContainer}>
+            <div style={{ marginBottom: 19 }}>
+              <label>About</label>
+            </div>
+            <div
+              className={`${classes.inputDiv} ${
+                editable.about && classes.active
+              }`}
+            >
+              <input
+                autoComplete="off"
+                value={values.about}
+                onChange={handleInputChange}
+                name="about"
+                type="text"
+                placeholder="Write about yourself"
+                disabled={!editable.about}
+              />
+              <button onClick={() => handleEditToggle("about")} title="Edit">
+                {editable.about ? <DoneIcon /> : <EditIcon />}
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
