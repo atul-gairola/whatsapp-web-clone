c146f1beae0b0177b7d23bd0d0af96cd179f2564
atul-gairola <atulgairola08@gmail.com>
2021-05-12 21:04:26 +0530

    Add Emojie Picker in User Profile

--- src/components/Sidebar/components/UserProfile.jsx
+++ src/components/Sidebar/components/UserProfile.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
-import { makeStyles, IconButton } from "@material-ui/core";
+import { makeStyles, IconButton, ClickAwayListener } from "@material-ui/core";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import { useAuth } from "../../../contexts/AuthContext";
 import CameraAltIcon from "@material-ui/icons/CameraAlt";
 import EditIcon from "@material-ui/icons/Edit";
 import DoneIcon from "@material-ui/icons/Done";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import { Picker } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css";
 
 const useStyles = makeStyles({
   container: {
@@ -47,7 +50,7 @@ const useStyles = makeStyles({
     display: "flex",
     flexDirection: "column",
     overflowX: "hidden",
-    overflowY: "auto"
+    overflowY: "auto",
   },
   imgContainer: {
     margin: "28px 0",
@@ -117,15 +120,15 @@ const useStyles = makeStyles({
     borderBottom: "2px solid #00bfa5",
   },
   info: {
-   padding: "0 40px",
-   marginBottom: "20px",
-   marginTop: "10px",
-   "& p": {
-       color: "rgba(0,0,0,0.45)",
-       lineHeight: "20px",
-       fontSize: 14
-   }
-  }
+    padding: "0 40px",
+    marginBottom: "20px",
+    marginTop: "10px",
+    "& p": {
+      color: "rgba(0,0,0,0.45)",
+      lineHeight: "20px",
+      fontSize: 14,
+    },
+  },
 });
 
 function UserProfile() {
@@ -141,6 +144,18 @@ function UserProfile() {
   const [editable, setEditable] = useState({
     name: false,
     about: false,
+    nameEmojie: false,
+    aboutEmojie: false,
+  });
+
+  const [emojiPickerStyle1, setEmojiPickerStyle1] = useState({
+    position: "absolute",
+    zIndex: 400,
+  });
+
+  const [emojiPickerStyle2, setEmojiPickerStyle2] = useState({
+    position: "absolute",
+    zIndex: 400,
   });
 
   //   handlers
@@ -160,94 +175,164 @@ function UserProfile() {
     }));
   };
 
+  const handleEmojiePicker = (e, inputName) => {
+    setEditable((prev) => ({
+      ...prev,
+      [inputName + "Emojie"]: true,
+    }));
+
+    inputName === "name"
+      ? setEmojiPickerStyle1((prev) => ({
+          ...prev,
+          left: e.pageX + 5 + "px",
+          top: e.pageY - 354 - 15 + "px",
+        }))
+      : setEmojiPickerStyle2((prev) => ({
+          ...prev,
+          left: e.pageX + 5 + "px",
+          top: e.pageY - 354 - 15 + "px",
+        }));
+  };
+
+  const handleClickAway = (name) => {
+    console.log("Click Away");
+    setEditable((prev) => ({
+      ...prev,
+      [name + "Emojie"]: false,
+    }));
+  };
+
+  const handleEmojiSelect = (emoji, inputName) => {
+    console.log(emoji);
+    setValues((prev) => ({
+      ...prev,
+      [inputName]: prev[inputName] + emoji.native,
+    }));
+  };
+
   return (
-    <div className={classes.container}>
-      <header className={classes.header}>
-        <div className={classes.headingContainer}>
-          <div style={{ width: "fit-content" }}>
-            <IconButton
-              style={{ width: "auto" }}
-              disableFocusRipple={true}
-              disableRipple={true}
-            >
-              <ArrowBackIcon />
-            </IconButton>
+    <>
+      {editable.nameEmojie && (
+        <ClickAwayListener onClickAway={() => handleClickAway("name")}>
+          <Picker
+            showPreview={false}
+            showSkinTones={false}
+            onSelect={(e) => handleEmojiSelect(e, "name")}
+            style={emojiPickerStyle1}
+          />
+        </ClickAwayListener>
+      )}
+      {editable.aboutEmojie && (
+        <ClickAwayListener onClickAway={() => handleClickAway("about")}>
+          <Picker
+            showPreview={false}
+            showSkinTones={false}
+            onSelect={(e) => handleEmojiSelect(e, "about")}
+            style={emojiPickerStyle2}
+          />
+        </ClickAwayListener>
+      )}
+      <div className={classes.container}>
+        <header className={classes.header}>
+          <div className={classes.headingContainer}>
+            <div style={{ width: "fit-content" }}>
+              <IconButton
+                style={{ width: "auto" }}
+                disableFocusRipple={true}
+                disableRipple={true}
+              >
+                <ArrowBackIcon />
+              </IconButton>
+            </div>
+            <h3>Profile</h3>
           </div>
-          <h3>Profile</h3>
-        </div>
-      </header>
-      <div className={classes.body}>
-        <div className={classes.imgContainer}>
-          <div className={classes.display}>
-            <div className={classes.overlay}>
-              <div>
-                <CameraAltIcon />
-                <p style={{ fontSize: 13, paddingTop: 7 }}>
-                  CHANGE <br /> PROILE PHOTO
-                </p>
+        </header>
+        <div className={classes.body}>
+          <div className={classes.imgContainer}>
+            <div className={classes.display}>
+              <div className={classes.overlay}>
+                <div>
+                  <CameraAltIcon />
+                  <p style={{ fontSize: 13, paddingTop: 7 }}>
+                    CHANGE <br /> PROILE PHOTO
+                  </p>
+                </div>
+              </div>
+              <img src={currentUser.img} alt={currentUser.name} />
+            </div>
+          </div>
+          <div className={classes.nameContainer}>
+            <div className={classes.inputContainer}>
+              <div style={{ marginBottom: 19 }}>
+                <label>Your Name</label>
+              </div>
+              <div
+                className={`${classes.inputDiv} ${
+                  editable.name && classes.active
+                }`}
+              >
+                <input
+                  autoComplete="off"
+                  value={values.name}
+                  onChange={handleInputChange}
+                  name="name"
+                  type="text"
+                  placeholder="Write your name"
+                  disabled={!editable.name}
+                />
+                {editable.name && (
+                  <InsertEmoticonIcon
+                    onClick={(e) => handleEmojiePicker(e, "name")}
+                    style={{ cursor: "pointer", marginRight: 5 }}
+                  />
+                )}
+                <button onClick={() => handleEditToggle("name")} title="Edit">
+                  {editable.name ? <DoneIcon /> : <EditIcon />}
+                </button>
               </div>
             </div>
-            <img src={currentUser.img} alt={currentUser.name} />
           </div>
-        </div>
-        <div className={classes.nameContainer}>
-          <div className={classes.inputContainer}>
-            <div style={{ marginBottom: 19 }}>
-              <label>Your Name</label>
-            </div>
-            <div
-              className={`${classes.inputDiv} ${
-                editable.name && classes.active
-              }`}
-            >
-              <input
-                autoComplete="off"
-                value={values.name}
-                onChange={handleInputChange}
-                name="name"
-                type="text"
-                placeholder="Write your name"
-                disabled={!editable.name}
-              />
-              <button onClick={() => handleEditToggle("name")} title="Edit">
-                {editable.name ? <DoneIcon /> : <EditIcon />}
-              </button>
-            </div>
+          <div className={classes.info}>
+            <p>
+              This is not your username or pin. This name will be visible to
+              your WhatsApp contacts.
+            </p>
           </div>
-        </div>
-        <div className={classes.info}>
-          <p>
-            This is not your username or pin. This name will be visible to your
-            WhatsApp contacts.
-          </p>
-        </div>
-        <div className={classes.aboutContainer}>
-          <div className={classes.inputContainer}>
-            <div style={{ marginBottom: 19 }}>
-              <label>About</label>
-            </div>
-            <div
-              className={`${classes.inputDiv} ${
-                editable.about && classes.active
-              }`}
-            >
-              <input
-                autoComplete="off"
-                value={values.about}
-                onChange={handleInputChange}
-                name="about"
-                type="text"
-                placeholder="Write about yourself"
-                disabled={!editable.about}
-              />
-              <button onClick={() => handleEditToggle("about")} title="Edit">
-                {editable.about ? <DoneIcon /> : <EditIcon />}
-              </button>
+          <div className={classes.aboutContainer}>
+            <div className={classes.inputContainer}>
+              <div style={{ marginBottom: 19 }}>
+                <label>About</label>
+              </div>
+              <div
+                className={`${classes.inputDiv} ${
+                  editable.about && classes.active
+                }`}
+              >
+                <input
+                  autoComplete="off"
+                  value={values.about}
+                  onChange={handleInputChange}
+                  name="about"
+                  type="text"
+                  placeholder="Write about yourself"
+                  disabled={!editable.about}
+                />
+
+                {editable.about && (
+                  <InsertEmoticonIcon
+                    onClick={(e) => handleEmojiePicker(e, "about")}
+                    style={{ cursor: "pointer", marginRight: 5 }}
+                  />
+                )}
+                <button onClick={() => handleEditToggle("about")} title="Edit">
+                  {editable.about ? <DoneIcon /> : <EditIcon />}
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
